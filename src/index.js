1c91e7298dbda0f5634580c68f6a20df98e0df02
baizk <baizk2017@126.com>
2019-06-05 18:48:13 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {hello} from "./helloword";
+
+document.write(hello())
+
+let a = b=>{
+	alert(111)
+}
+a();
---
+++ src/search.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import ReactDom from 'react-dom'
+import './search.less';
+
+class Search extends React.Component{
+	render() {
+		return <div className="search-text">search text</div>;
+	}
+}
+
+ReactDom.render(
+	<Search />,
+	document.getElementById('root')
+)
